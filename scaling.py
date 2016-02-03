122f71aa9034a8d46ec724fd9449337ee4f32621
ihstevenson <ian.h.stevenson@gmail.com>
2016-02-03 13:03:22 -0500

    minor adjustments to time calculation and for clarity

--- scaling.py
+++ scaling.py
@@ -1,13 +1,10 @@
 import pandas
-import urllib
 from matplotlib import pyplot
 from matplotlib.ticker import ScalarFormatter, LogLocator
 from scipy import linspace, polyval, polyfit, stats
 from numpy import double, log, asarray, exp, std, sqrt, mean
 from pylab import random_integers, zeros
 import seaborn as sns
-import statsmodels.api as sm
-from statsmodels.stats.outliers_influence import summary_table
 
 def log_10_product(x, pos):
     """The two args are the value and tick position.
@@ -22,7 +19,8 @@ data = [];
 data = pandas.io.parsers.read_csv("https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=0Ai7vcDJIlD6AdF9vQWlNRDh2S1dub09jMWRvTFRpemc&single=true&gid=0&range=A2%3AC1000&output=csv")
 
 data = asarray(data);
-t = double(data[:,0])+double(data[:,1])/12;
+data[data[:,1]==0,1]=6.5 # fix missing month values by assuming middle of year
+t = double(data[:,0])+double(data[:,1]-0.5)/12; # assume middle of month
 n = double(data[:,2]);
 
 # Only keep first M papers to record >=N neurons
@@ -34,7 +32,6 @@ for i in range(0,len(t)):
 idx = asarray(idx)
 
 f = pyplot.figure();
-
 pyplot.grid(b=True, which='major', color='0.9', linestyle='-')
 pyplot.tick_params(axis='both', direction='out')
 pyplot.locator_params(axis='x',nbins=16)
@@ -43,19 +40,20 @@ pyplot.semilogy(t[idx],n[idx],'o',ms=10,mew=0.25,mec='1.0')
 pyplot.xlabel('Publication Date',fontsize=16)
 pyplot.ylabel('Simultaneously Recorded Neurons',fontsize=16)
 
+# Exponential fit with bootstrap standard error (could use statsmodels here)
 (ar,br)=polyfit(t[idx],log(n[idx]),1)
 arb=zeros(1000)
 brb=zeros(1000)
 for i in range(0,1000):
 	sidx = idx[random_integers(0, len(idx)-1, len(idx))]
 	(arb[i],brb[i])=polyfit(t[sidx],log(n[sidx]),1)
-x = t[idx];
-y = log(n[idx])
-yhat = br+x*ar;
 
 # Add exponential fit to plot
 t0 = linspace(min(t)-1,max(t)+1,100);
 nhat = polyval([ar,br],t0);
+x = t[idx];
+y = log(n[idx])
+yhat = br+x*ar;
 ci = stats.t.isf(0.05/2,len(x)-2)*sqrt(sum(pow(y-yhat,2))/(len(y)-2))*sqrt(1/len(y)+pow(t0-mean(x),2)/sum(pow(x-mean(x),2)))
 pyplot.semilogy(t0,exp(nhat),lw=2)
 pyplot.fill_between(t0,exp(nhat+ci),exp(nhat-ci),alpha=0.25,color=sns.color_palette()[1])
@@ -65,7 +63,6 @@ pyplot.xlim([min(t)-1, max(t)+1])
 
 pyplot.title("Doubling Time: %0.01f $\pm$ %0.01f years (n=%i)" % (log(2)/ar,std(log(2)/arb),len(idx)),fontsize=16)
 sns.despine()
-sns.set_style
 pyplot.gca().yaxis.set_major_locator(LogLocator(subs=[0.5,1.0]))
 pyplot.gca().yaxis.set_major_formatter(ScalarFormatter())
 
